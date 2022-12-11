9e07c94ac3e5a479ba1630abdd0f07061978a1ae
Andrei Cozma <me@andreicozma.com>
2022-12-10 17:34:07 -0700

    Quick update/fix

--- main_eval.py
+++ main_eval.py
@@ -1,71 +1,26 @@
 import collections
 
 import argparse
-import time
 
 import numpy as np
-import torch
 from tqdm import tqdm
 
-from rllib.Utils import discount_rewards, load_model
-
-
-def eval_model(episode_num, args, model_params={}):
-    checkpoint_path = args.file_path
-    model, model_name = load_model(checkpoint_path, model_params)
-
-    # setup environemnt
-    done = False
-    state = torch.FloatTensor(model.env.reset())
-    total_rewards, total_steps = 0, 0
-    pred_value = []
-    hist_rewards = []
-    while not done:
-        state = state.to(device=model.device)
-        next_state, value, reward, done = None, None, None, None
-        # api for models are a bit different for getting state and value
-        if model_name == "ppo":
-            with torch.no_grad():
-                pi, action, value = model(state)
-            next_state, reward, done, _ = model.env.step(action.cpu().numpy())
-
-        elif model_name == "a2c":
-            with torch.no_grad():
-                action = model.agent(state, model.device)[0]
-                _, value = model.net(state.reshape((1, -1)))
-            next_state, reward, done, _ = model.env.step(action.cpu().numpy())
-
-        hist_rewards.append(reward)
-        pred_value.append(value)
-
-        # Render the env
-        model.env.render()
-
-        # Wait a bit before the next frame unless you want to see a crazy fast video
-        time.sleep(0.001)
-        state = torch.FloatTensor(next_state)
-
-    actual_return = discount_rewards(hist_rewards, model.gamma)
-
-    # close the render
-    model.env.close()
-    return {
-            "total_reward" : np.sum(hist_rewards),
-            "total_steps"  : total_steps,
-            'actual_return': actual_return,
-            'pred_value'   : pred_value
-    }
+from rllib.Utils import Model
 
 
 def main():
     args = parse_args()
     running_rew_len = args.running_rew_len
     num_episodes = args.num_episodes
+    checkpoint_path = args.file_path
+
+    m = Model()
 
     running_reward: collections.deque = collections.deque(maxlen=running_rew_len)
-    tq_episode_iter = tqdm(range(num_episodes), leave=False, description="Episode")
-    for episode_number in tq_episode_iter:
-        metrics = eval_model(episode_number, args)
+    tq_episode_iter = tqdm(range(num_episodes), leave=False, desc="Episode")
+    for _ in tq_episode_iter:
+        m.load_model(checkpoint_path)
+        metrics = m.eval()
         running_reward.append(metrics['total_reward'])
         metrics |= {
                 "running_reward": np.mean(running_reward),
@@ -77,11 +32,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument("--env", type=str, default="LunarLander-v2")
     parser.add_argument("-f", "--file_path", type=str, required=True)
-    parser.add_argument("-ne", "--num_episodes", type=int, default=1)
+    parser.add_argument("-ne", "--num_episodes", type=int, default=5)
     parser.add_argument("--running_rew_len", type=int, default=50)
-    # Logging and metrics
-    parser.add_argument("--log_dir", type=str, default="logs/")
-    parser.add_argument("--wandb_project", type=str, default="rl_project")
     return parser.parse_args()
 
 
--- main_train.py
+++ main_train.py
@@ -1,63 +1,30 @@
 import argparse
-import os
 
-import torch
-from pytorch_lightning import Trainer
-
-from rllib.Utils import get_logger, get_model, get_model_hash
-
-
-def train(args, model_params={}):
-    env = args.env
-    model_name = args.model_name
-    num_epochs = args.num_epochs
-    checkpoint_dir = args.checkpoint_dir
-    wandb_proj = args.wandb_project
-    log_dir = args.log_dir
-
-    model = get_model(env, model_name, model_params)
-
-    model_hash = get_model_hash(model)
-    logger = get_logger(wandb_proj, log_dir, model_hash, model_name)
-
-    trainer = Trainer(
-            accelerator="auto",
-            devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
-            max_epochs=num_epochs,
-            val_check_interval=args.val_check_interval,
-            logger=logger,
-            enable_checkpointing=True,
-            auto_lr_find=True,
-            auto_scale_batch_size=True,
-            enable_model_summary=True,
-            precision=16,
-            # callbacks=callbacks
-    )
-
-    # train
-    trainer.fit(model)
-    model_save_dir = os.path.join(checkpoint_dir, model_name, env)
-    os.makedirs(model_save_dir, exist_ok=True)
-    save_path = os.path.join(model_save_dir, f"{model_hash}.pt")
-    print(save_path)
-    torch.save(model.state_dict(), save_path)
+from rllib.Utils import Model
 
 
 def main():
     args = parse_args()
-    train(args)
+    env = args.env
+    model_name = args.model_name
+    num_epochs = args.num_epochs
+    wandb_proj = args.wandb_project
+    val_check_interval = args.val_check_interval
+
+    m = Model()
+    m.create_model(env, model_name)
+    m.create_wandb_logger(wandb_proj)
+    m.train(num_epochs, val_check_interval)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env", type=str, default="LunarLander-v2")
-    parser.add_argument("-nm", "--num_models", type=int, default=10)
-    parser.add_argument("-m", "--model_name", type=str, default="ppo")
+    parser.add_argument("-m", "--model_name", type=str, default="ppo_ex")
+    parser.add_argument("-nm", "--num_models", type=int, default=5)
     parser.add_argument("-ne", "--num_epochs", type=int, default=150)
     parser.add_argument("--val_check_interval", type=int, default=25)
-    parser.add_argument("--log_dir", type=str, default="logs/")
     parser.add_argument("--wandb_project", type=str, default="rl_project")
-    parser.add_argument("--checkpoint_dir", type=str, default="checkpoints/")
     return parser.parse_args()
 
 
